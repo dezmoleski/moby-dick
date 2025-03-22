f1cafa2b7d667a4b02e2c8a78d172ecb998ba513
Dez Moleski <dez@moleski.com>
2025-03-22 11:59:07 -0700

    Initial chapter split complete incl. Etymology & Extracts

--- split-pg2701-txt.py
+++ split-pg2701-txt.py
@@ -4,8 +4,16 @@
 # plain text file of Moby-Dick into chapters and sentences. Output
 # is plain text also in a simple format for downstream processing.
 # 
-# Copyright (C) 2025 Dez Moleski dez@moleski.com
+# The script is tuned to work with a specially (hopefully slightly)
+# modified version of the plain text edition #2701 from Project
+# Gutenberg, which is archived with the script here:
+# 
+#   https://github.com/dezmoleski/moby-dick
+# 
+# This script is Copyright (C) 2025 Dez Moleski dez@moleski.com
 # MIT License: All uses allowed with attribution.
+# 
+# The output of this script is in the public domain.
 # 
 import sys
 import re
@@ -30,7 +38,7 @@ class Chapter:
     def __repr__(self) -> str:
         return f'Ch. {self.number} "{self.title}" {self.text_offset} - {self.end_offset}'
     
-    def guts(self) -> str:
+    def text(self) -> str:
         if len(self) > 0:
             return MOBY_STR[self.text_offset:self.end_offset]
         return ''
@@ -40,16 +48,56 @@ def split_moby_dick(filepath: str):
     
     with open(filepath, 'rt') as f:
         MOBY_STR = f.read()
-    print("# Split Moby-Dick from file=", filepath, " len(file.read())=", len(MOBY_STR))
-
+    print("# Split Moby-Dick into chapters, paragraphs, and sentences from file:", filepath, " len:", len(MOBY_STR))
+    print("# The contents of this file are in the public domain.")
+    
     ##########################
     # PHASE I - Find chapters
-    # 
+    ##########################
+    contents = dict() # {<chapter number>:"Chapter Title", ...}
+    chapters = list() # of Chapter instances
+    
+    # We have a couple special cases right off the bat: the ETYMOLOGY and EXTRACTS.
+    # My interest here is to catalog the sentences that are original to Melville,
+    # so the sections other than the intro paragraphs are not part of the main
+    # index. The plan though is to keep all the text of the book in the output, so
+    # there is going to be some **TBD** markup for pre-formatted sections that are
+    # not included in the chapter/paragraph/sentence index.
+    etymology_re = r'^ETYMOLOGY .+\.$'
+    re_etymology = re.compile(etymology_re, re.MULTILINE)
+    etym = None
+    ETYM_INDEX = -1
+    for etym_match in re_etymology.finditer(MOBY_STR):
+        title = etym_match.group()[0:-1] # Take the whole matching string minus the period.
+        if contents.get(ETYM_INDEX) is None:
+            contents[ETYM_INDEX] = title
+        else:
+            etym = Chapter(ETYM_INDEX,
+                           title,
+                           etym_match.start(),
+                           etym_match.end()+2) # Skip two newlines to reach chapter text offset
+            chapters.append(etym)
+    
+    extracts_re = r'^EXTRACTS .+\.$'
+    re_extracts = re.compile(extracts_re, re.MULTILINE)
+    extr = None
+    EXTR_INDEX = 0
+    for extr_match in re_extracts.finditer(MOBY_STR):
+        title = extr_match.group()[0:-1] # Take the whole matching string minus the period.
+        if contents.get(EXTR_INDEX) is None:
+            contents[EXTR_INDEX] = title
+        else:
+            extr = Chapter(EXTR_INDEX,
+                           title,
+                           extr_match.start(),
+                           extr_match.end()+2) # Skip two newlines to reach chapter text offset
+            etym.end_offset = extr.title_offset - 1
+            chapters.append(extr)
+    
+    # The main chapters 1-135 can be chopped up with this regex:
     chapter_re = r'^CHAPTER (?P<ch_num>\d+)\. (?P<ch_title>.+)(?P<title_terminator>[\.\!\?])$'
     re_chapter = re.compile(chapter_re, re.MULTILINE)
-    contents = dict()
-    chapters = list()
-    prev_chapter = None
+    prev_chapter = extr
     for ch_match in re_chapter.finditer(MOBY_STR):
         ch_num = int(ch_match['ch_num'])
         ch_title = ch_match['ch_title']
@@ -65,7 +113,7 @@ def split_moby_dick(filepath: str):
             ch = Chapter(ch_num,
                          ch_title,
                          ch_match.start(),
-                         ch_match.end()+2) # Skip two newlines to reach chapter guts offset
+                         ch_match.end()+2) # Skip two newlines to reach chapter text offset
             if prev_chapter is not None:
                 prev_chapter.end_offset = ch.title_offset - 1
             chapters.append(ch)
@@ -81,7 +129,7 @@ def split_moby_dick(filepath: str):
             ep = Chapter(136,
                          "Epilogue",
                          ep_match.start(),
-                         ep_match.end()+2) # Skip two newlines to reach chapter guts offset
+                         ep_match.end()+2) # Skip two newlines to reach chapter text offset
             prev_chapter.end_offset = ep.title_offset - 1
             chapters.append(ep)
             
@@ -93,6 +141,7 @@ def split_moby_dick(filepath: str):
     # Debug output
     for chap in chapters:
         print(chap)
+        print(chap.text())
 
     print("N chapters:", len(chapters))
     
